026f6ded212df069867da6c03c107f0b22b659d6
Sambhav Shrestha <sambhavshrestha1@gmail.com>
2021-03-07 15:57:49 -0500

    removed unnecessary imports

--- app/src/main/java/com/example/yearbookproject/SambhavActivity.kt
+++ app/src/main/java/com/example/yearbookproject/SambhavActivity.kt
@@ -1,20 +1,12 @@
 package com.example.yearbookproject
 
-import android.content.Context
 import android.content.Intent
-import android.graphics.Bitmap
-import android.graphics.drawable.BitmapDrawable
 import android.net.Uri
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.FileProvider.getUriForFile
-import java.io.File
-import java.io.FileOutputStream
 import java.io.InputStream
-import java.security.AccessController.getContext
+
 
 
 class SambhavActivity : AppCompatActivity() {
